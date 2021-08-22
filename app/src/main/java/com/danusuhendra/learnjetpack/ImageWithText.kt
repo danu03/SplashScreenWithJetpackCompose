d1c994b073560d0d54fdb93af90f7a0112de8f6d
Danu Suhendra <danusuhendra03@gmail.com>
2021-08-23 00:27:01 +0700

    instagram clone UI - post section

--- app/src/main/java/com/danusuhendra/learnjetpack/ImageWithText.kt
+++ app/src/main/java/com/danusuhendra/learnjetpack/ImageWithText.kt
@@ -2,7 +2,7 @@ package com.danusuhendra.learnjetpack
 
 import androidx.compose.ui.graphics.painter.Painter
 
-data class StoryHighlight(
+data class ImageWithText(
     val image: Painter,
     val text: String
 )
